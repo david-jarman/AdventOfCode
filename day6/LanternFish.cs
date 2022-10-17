fdc28aba755ac5111e8b7c1e85894c2d397d32ce
David Jarman <davidbjarman@gmail.com>
2022-10-16 21:56:09 -0700

    Day 6, part 2

--- day6/LanternFish.cs
+++ day6/LanternFish.cs
@@ -12,10 +12,38 @@ public class LanternFish
             .Select(s => int.Parse(s))
             .ToList();
 
+        //const int days = 80;
+        const int days = 256;
+
+        // Array representing the count of fish in each "age", 0 -> 8
+        long[] populationAges = new long[9];
+
+        // Initialize based on input
+        foreach (int fishAge in lanternFish)
+        {
+            populationAges[fishAge]++;
+        }
+
+        // Simulate each day
+        for (int i = 0; i < days; i++)
+        {
+            // The age 0 fish are the repoducers
+            long reproducers = populationAges[0];
+
+            // Age all fish by 1 day by shifting them down one index, and add reproduced fish to the end
+            populationAges = populationAges.Skip(1).Take(8).Append(reproducers).ToArray();
+
+            // Reset the age of the reproducers back to 6
+            populationAges[6] += reproducers;
+        }
+
+        Console.WriteLine($"Total count: {populationAges.Sum()}");
+    }
+
+    public void Simulate(List<int> lanternFish, int days)
+    {
         List<int> spawnedFish = new List<int>();
 
-        //const int days = 18;
-        const int days = 80;
         for (int day = 0; day < days; day++)
         {
             // simulate
@@ -36,6 +64,8 @@ public class LanternFish
             // Add spawned fish
             lanternFish.AddRange(spawnedFish);
             spawnedFish = new List<int>();
+
+            Console.WriteLine($"Day: {day+1}, Count: {lanternFish.Count}, School: {string.Join(',', lanternFish)}");
         }
 
         Console.WriteLine($"School size: {lanternFish.Count}");
